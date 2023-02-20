d91a34e1806c4ae635acb40cf0a6042d8c81ba88
Ziru Zou <jiyucoding@yahoo.com>
2023-02-20 09:20:15 -0500

    create property object 'value' with get_value & set_value as args, save string in self._value if string is a str type

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  
+  def __init__(self, value=None):
+    self._value = value
+
+
+  def get_value(self):
+    return self._value
+
+  
+  def set_value(self, string):
+    if type(string) == str:
+      self._value = string
+    else:
+      print("The value must be a string.")
+
+
+  value = property(get_value, set_value)
